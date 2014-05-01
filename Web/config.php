50dce8ddbb4c162d6c7e6aa3e903c19e662eaa8b
Benjamin Häublein <BenjaminHae@users.noreply.github.com>
2014-05-01 19:31:25 +0200

    Web: eintragen ohne Position möglich. Positionsausgaben filtern auf Einträge mit Position

--- Web/config.php
+++ Web/config.php
@@ -2,14 +2,14 @@
 class syDB{
 	private $host, $user, $pwd;
 
-   private $connection;
-   function __construct() {
+	private $connection;
+	function __construct() {
 		include('private.php');
 		$this->connection=mysql_connect($this->host, $this->user, $this->pwd) or die ("Verbindungsversuch fehlgeschlagen");
 		mysql_select_db($this->db, $this->connection) or die("Konnte die Datenbank nicht waehlen.");
-   }
-   public function Insert($gp, $pos, $add, $dir, $trans, $comm)
-   {
+	}
+	public function Insert($gp, $pos, $add, $dir, $trans, $comm)
+	{
 		settype($gp, 'integer');
 		if ($gp<=0)
 		   die("Keine Gruppennummer angegeben");
@@ -19,7 +19,7 @@ class syDB{
 			$gp -= 690;
 		$pos = mysql_real_escape_string(trim($pos));
 		$add = mysql_real_escape_string(trim($add));
-		if (($pos == "") and ($add == ""))
+		if ($pos == "")
 		   die("Kein Standort angegeben");
 		$dir = mysql_real_escape_string(trim($dir));
 		$trans = mysql_real_escape_string(trim($trans));
@@ -28,16 +28,16 @@ class syDB{
 		$comm = mysql_real_escape_string(trim($comm));
 		$sql = "INSERT INTO scotland_yard VALUES ($gp,'$pos','$add','$dir','$trans','$comm', DEFAULT);";
 		return mysql_query($sql, $this->connection) or die("Konnte den Datensatz nicht eintragen.");
-   }
-   public function GetGroupPosition($gp)
-   {
+	}
+	public function GetGroupPosition($gp)
+	{
 		settype($gp, 'integer');
-		$sql = "SELECT * FROM scotland_yard WHERE scotland_yard.group = $gp ORDER BY timestamp DESC LIMIT 1;";
+		$sql = "SELECT * FROM scotland_yard WHERE scotland_yard.group = $gp AND NOT scotland_yard.position = '' ORDER BY timestamp DESC LIMIT 1;";
 		$query = mysql_query($sql, $this->connection);
 		return mysql_fetch_array($query);
-   }
-   public function GetGroupMovement($gp, $limit=0)
-   {
+	}
+	public function GetGroupMovement($gp, $limit=0)
+	{
 		settype($gp, 'integer');
 		settype($limit, 'integer');
 		$limString="";
@@ -45,16 +45,16 @@ class syDB{
 		{
 		   $limString=" LIMIT ".$limit;
 		}
-		$sql = "SELECT * FROM scotland_yard WHERE scotland_yard.group = $gp ORDER BY timestamp DESC$limString;";
+		$sql = "SELECT * FROM scotland_yard WHERE scotland_yard.group = $gp AND NOT scotland_yard.position = '' ORDER BY timestamp DESC$limString;";
 		$query = mysql_query($sql, $this->connection);
 			while($r=mysql_fetch_array($query))
 			{
 				$res[]=$r;
 			}
 		return $res;
-   }
-   public function GetGroupIDs()
-   {
+	}
+	public function GetGroupIDs()
+	{
 		$sql = "SELECT DISTINCT scotland_yard.group FROM scotland_yard ORDER BY scotland_yard.group;";
 		$query = mysql_query($sql, $this->connection);
 		while($r=mysql_fetch_array($query))
@@ -62,9 +62,9 @@ class syDB{
 			$res[]=$r[0];
 		}
 		return $res;
-   }
-   public function GetCommentsBy($gp, $limit=0)
-   {
+	}
+	public function GetCommentsBy($gp, $limit=0)
+	{
 		settype($gp,'integer');
 		settype($limit, 'integer');
 		$limString="";
@@ -79,9 +79,9 @@ class syDB{
 			$res[]=$r;
 		}
 		return $res;
-   }
-   public function GetAllComments($limit=0)
-   {
+	}
+	public function GetAllComments($limit=0)
+	{
 		settype($limit, 'integer');
 		$limString="";
 		if ($limit>0)
@@ -95,6 +95,6 @@ class syDB{
 			$res[]=$r;
 		}
 		return $res;
-   }
+	}
 }
 ?>
