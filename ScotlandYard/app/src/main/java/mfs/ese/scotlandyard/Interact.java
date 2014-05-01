bf9e07e048a5717d9c0b1aa975ac56373cf052ab
Benjamin Häublein <BenjaminHae@users.noreply.github.com>
2014-05-01 19:30:34 +0200

    Interact jetzt ohne Fragmente

--- ScotlandYard/app/src/main/java/mfs/ese/scotlandyard/Interact.java
+++ ScotlandYard/app/src/main/java/mfs/ese/scotlandyard/Interact.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.LayoutInflater;
@@ -20,6 +21,7 @@ import android.widget.TextView;
 public class Interact extends Activity implements HttpResp{
     public HttpResp resp = this;
     private String state = "allComments";
+    private Resources mResources;
 
     public void refresh()
     {
@@ -40,6 +42,15 @@ public class Interact extends Activity implements HttpResp{
                 outputText +=generateCommentHTML(groupVals[0],groupVals[6],groupVals[5]) + "<br/>";
             }
             ((TextView) findViewById(R.id.commentText)).setText(Html.fromHtml(outputText));
+        }
+        else if (url.equals(mResources.getString(R.string.URL_ins)))
+        {
+            if (resp.equals("OK")) {
+                refresh();
+                ((EditText) findViewById(R.id.editComment)).setText("");
+            } else {
+                ((TextView) findViewById(R.id.commentText)).setText(Html.fromHtml(resp));
+            }
         }
     }
 
@@ -62,15 +73,13 @@ public class Interact extends Activity implements HttpResp{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interact);
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-        final Button buttonSend = (Button) findViewById(R.id.SendCommentButton);//AutoTracking aktivieren
+        mResources = this.getResources();
+
+        final Button buttonSend = (Button) findViewById(R.id.sendComment);//AutoTracking aktivieren
         buttonSend.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
+                EditText text = (EditText) findViewById(R.id.editComment);
+                Vars.SendLocation(0, text.getText().toString(), "", "", "", resp);//TODO gpid setzen
             }
         });
     }
@@ -101,22 +110,6 @@ public class Interact extends Activity implements HttpResp{
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
-        }
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_interact, container, false);
-            return rootView;
         }
     }
 }
